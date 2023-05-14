5a96113f22f1a619a02b98fe605684f2c8df15dc
ChanWooong <oco466kr@knu.ac.kr>
2023-05-14 23:05:11 +0900

    add kmp algorithm and make display

--- grep0.c
+++ grep0.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
-int main(){
-	printf("grep");
+#include <string.h>
+#include <stdlib.h>
+#include "grepLib.h"
+
+int main(void)
+{
+    char* text = "ABCABAABCABAC";
+    char* pattern = "CAB";
+ 
+    int n = strlen(text);
+    int m = strlen(pattern);
+ 
+    KMP(text, pattern, n, m);
+ 
+    return 0;
 }
+
---
+++ grepLib.h
@@ -0,0 +1,2 @@
+void KMP(const char* text, const char* pattern, int m, int n);
+
