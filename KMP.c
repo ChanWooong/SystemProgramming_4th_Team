16c0712d6d6e5ea55da568b75810f6298e6c8e8c
ChanWooong <oco466kr@knu.ac.kr>
2023-05-25 15:01:46 +0900

    new branch

--- KMP.c
+++ KMP.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
- 
-// KMP 알고리즘을 구현하는 함수
-void KMP(const char* text, const char* pattern, int m, int n)
+
+int result[BUFSIZ] = {0};
+int* KMP(const char* text, const char* pattern, int m, int n)
 {
 	int arr[20];
 	int cnt = 0;
-    // 기본 사례 1: 패턴이 NULL이거나 비어 있음
+
     if (*pattern == '\0' || n == 0) {
         printf("The pattern occurs with shift 0");
     }
  
-    // 기본 경우 2: 텍스트가 NULL이거나 텍스트의 길이가 패턴의 길이보다 작음
+
     if (*text == '\0' || n > m) {
         printf("Pattern not found");
     }
  
-    // next[i]는 다음으로 가장 좋은 부분 일치의 인덱스를 저장합니다.
+
     int next[n + 1];
  
     for (int i = 0; i < n + 1; i++) {
@@ -48,13 +48,15 @@ void KMP(const char* text, const char* pattern, int m, int n)
         else if (j > 0)
         {
             j = next[j];
-            i--;    // `i`는 다음 반복에서 증가할 것이기 때문에
+            i--;   
         }
     }
+    int check = 0;
     cnt = 0;
     for(int k = 0; k < m; k++){
 	if(k == arr[cnt]){
-		printf("[");	
+		printf("[");
+		result[check++] = k;	
 	}
 	if(k == arr[cnt]+n){
 		printf("]");
@@ -64,5 +66,9 @@ void KMP(const char* text, const char* pattern, int m, int n)
 	}
 	printf("%c",text[k]);
     }
+    if(check == 0){
+	printf("There has no String in FIle\n");
+    }	
     printf("\n");
+    return result;
 }
--- grep0.c
+++ grep0.c
@@ -5,29 +5,49 @@
 #include <unistd.h>
 #include <fcntl.h>
 #define MAX 3000
+void displayScreen(FILE* fd, char* find);
 
 int main(int argc, char* argv[])
 {   
     FILE* fd;
-    char buffer[MAX] = {0};
     char find[MAX];
+    char option;
     
-    if(argc != 3){
+    if(argc < 3){
     	printf("check the number of parameters.\n");
 	exit(1);
     }
-    fd = fopen(argv[2], "r");
-    
+    if(argc == 3){
+    	fd = fopen(argv[2], "r");
+    	strcpy(find,argv[1]);
+    	displayScreen(fd, find);
+    }
+    if(argc == 4){
+    	option = argv[1][1];
+    	fd = fopen(argv[3], "r");
+    	strcpy(find,argv[2]);
+    	displayScreen(fd, find);
+    }
+
+    return 0;
+}
+
+void displayScreen(FILE* fd, char* find){
+    int data[MAX] = {0};
+    char buffer[MAX] = {0};
     fread(buffer, 1, MAX, fd);
-    strcpy(find,argv[1]);
+
     printf("---------------- origin ----------------\n\n");
     printf("%s", buffer);
  
     int n = strlen(buffer);
     int m = strlen(find);
     printf("\n\n---------------- fine ----------------\n\n");
-    KMP(buffer, find, n, m);
+    data = KMP(buffer, find, n, m);
+    printf("\n");
+    for(int i = 0; i < sizeof(data)/4; i++){
+    	printf("[%d]", data[i]);
+    }
  
-    return 0;
 }
 
