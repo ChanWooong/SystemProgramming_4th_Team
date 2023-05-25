172797b5f3a4cf959169e105ec02403ae4b3d540
ChanWooong <104729817+ChanWooong@users.noreply.github.com>
2023-05-25 16:14:13 +0900

    Update grep0.c
    change output in grep

--- grep0.c
+++ grep0.c
@@ -1,3 +1,4 @@
+내 드라이브
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -5,29 +6,61 @@
 #include <unistd.h>
 #include <fcntl.h>
 #define MAX 3000
+void displayScreen(FILE* fd, char* find);
 
 int main(int argc, char* argv[])
 {   
     FILE* fd;
-    char buffer[MAX] = {0};
     char find[MAX];
+    char option;
     
-    if(argc != 3){
+    if(argc < 3){
     	printf("check the number of parameters.\n");
 	exit(1);
     }
-    fd = fopen(argv[2], "r");
-    
-    fread(buffer, 1, MAX, fd);
-    strcpy(find,argv[1]);
-    printf("---------------- origin ----------------\n\n");
-    printf("%s", buffer);
- 
-    int n = strlen(buffer);
-    int m = strlen(find);
-    printf("\n\n---------------- fine ----------------\n\n");
-    KMP(buffer, find, n, m);
- 
+    if(argc == 3){
+    	fd = fopen(argv[2], "r");
+    	strcpy(find,argv[1]);
+    	displayScreen(fd, find);
+    }
+    if(argc == 4){
+    	option = argv[1][1];
+    	fd = fopen(argv[3], "r");
+    	strcpy(find,argv[2]);
+    	displayScreen(fd, find);
+    }
+
     return 0;
 }
 
+void displayScreen(FILE* fd, char* find){
+    int *data;
+    char buffer[MAX] = {0};
+    fread(buffer, 1, MAX, fd);
+
+    printf("---------------- origin ----------------\n\n");
+    printf("%s", buffer);
+ 
+    int n = strlen(find);
+    int m = strlen(buffer);
+    printf("\n\n---------------- fine ----------------\n\n");
+    data = KMP(buffer, find, m, n);
+    int point;
+    
+    for(int k = 0; k < m; k++){
+	if(k == data[point]){
+		printf("[");	
+	}
+	if(k == data[point]+n){
+		printf("]");
+		k--;
+		point++;
+		continue;
+	}
+	printf("%c",buffer[k]);
+    }
+    
+    printf("\n");
+    
+ 
+}
