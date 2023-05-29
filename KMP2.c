a3be54986a084b2a403fe7fb92bf1bc114e79526
dnn12 <wjddn3078@naver.com>
2023-05-30 02:26:26 +0900

    modify print pre, next_line

---
+++ KMP2.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define COLOR_DEFAULT "\033[49m"
+#define COLOR_ORANGE "\033[43m"
+
+// KMP 알고리즘을 구현하는 함수
+void KMP(const char* text, const char* pattern, int m, int n)
+{
+	int arr[20];
+	int cnt = 0;
+    // 기본 사례 1: 패턴이 NULL이거나 비어 있음
+    if (*pattern == '\0' || n == 0) {
+        printf("The pattern occurs with shift 0");
+    }
+ 
+    // 기본 경우 2: 텍스트가 NULL이거나 텍스트의 길이가 패턴의 길이보다 작음
+    if (*text == '\0' || n > m) {
+        printf("Pattern not found");
+    }
+ 
+    // next[i]는 다음으로 가장 좋은 부분 일치의 인덱스를 저장합니다.
+    int next[n + 1];
+ 
+    for (int i = 0; i < n + 1; i++) {
+        next[i] = 0;
+    }
+ 
+    for (int i = 1; i < n; i++)
+    {
+        int j = next[i];
+ 
+        while (j > 0 && pattern[j] != pattern[i]) {
+            j = next[j];
+        }
+ 
+        if (j > 0 || pattern[j] == pattern[i]) {
+            next[i + 1] = j + 1;
+        }
+    }
+ 
+    for (int i = 0, j = 0; i < m; i++)
+    {
+        if (*(text + i) == *(pattern + j))
+        {
+            if (++j == n) {
+               arr[cnt++] = i-j+1;
+            }
+        }
+        else if (j > 0)
+        {
+            j = next[j];
+            i--;    // `i`는 다음 반복에서 증가할 것이기 때문에
+        }
+    }
+    cnt = 0;
+    for(int k = 0; k < m; k++){
+	if(k == arr[cnt]){
+		printf(COLOR_ORANGE);	
+	}
+	if(k == arr[cnt]+n){
+		printf(COLOR_DEFAULT);
+		cnt++;
+		k--;
+		continue;
+	}
+	printf("%c",text[k]);
+    }
+    printf(COLOR_DEFAULT);
+    //printf("\n");
+}
---
+++ grep2.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "grepLib.h"
+#include <stdbool.h>
+#include <signal.h>
+#include <ctype.h>
+#include <termios.h>
+#include <unistd.h>
+#define BUFFERSIZ 3
+#define MAX 3000
+
+volatile sig_atomic_t flag = 0;
+
+void signal_handler(int signal) {
+	if(signal == SIGINT)
+		flag = 1;
+}
+
+#ifdef _WIN32
+void clear() {
+	system("cls");
+}
+#else
+void clear() {
+	printf("\033[H\033[J");
+}
+#endif
+
+
+void setup() {
+	struct sigaction sa;
+	sa.sa_handler = signal_handler;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGINT, &sa, NULL);
+}
+
+void set_mode(int fd, struct termios* prev_term) {
+	struct termios term;
+	tcgetattr(fd, prev_term);
+	term = *prev_term;
+	term.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(fd, TCSANOW, &term);
+}
+
+void restore(int fd, struct termios* prev_term) {
+	tcsetattr(fd, TCSANOW, prev_term);
+}
+
+int getch() {
+	int ch;
+	struct termios old_term;
+	set_mode(STDIN_FILENO, &old_term);
+	ch = getchar();
+	restore(STDIN_FILENO, &old_term);
+	return ch;
+}
+
+
+int main(int argc, char* argv[])
+{
+    FILE* fd;
+    char cur_line[MAX] = {0};
+    char find[MAX];
+    char pre_line[BUFFERSIZ][MAX] = {0};
+    char next_line[MAX] = {0};
+    int index = 0;
+    bool found = false;
+    bool skip = false;
+    bool print_cur_line = false;
+    bool print_next_line = false;
+    bool input = false;
+
+    if (argc != 3)
+    {
+        printf("Check the number of parameters.\n");
+        exit(1);
+    }
+
+    fd = fopen(argv[2], "r");
+
+    if (fd == NULL)
+    {
+        printf("Cannot open file\n");
+        exit(1);
+    }
+
+    setup();
+
+   
+    while (fgets(cur_line, MAX, fd) != NULL)
+    {	
+	clear();
+
+	strcpy(find, argv[1]);
+
+        if (strstr(cur_line, argv[1]) != NULL) {
+	
+            found = true;
+	    skip = true;
+
+            int n = strlen(cur_line);
+            int m = strlen(find);
+	    
+	    
+	    printf("\n\n");
+
+	    printf("print pre line\n\n");
+
+	    KMP(cur_line, find, n, m);
+	    printf("\n");
+
+	    strncpy(pre_line[index], cur_line, MAX);       
+	    index = (index+1)%BUFFERSIZ;
+	    
+	    if(fgets(next_line, MAX, fd)!= NULL) {
+		   printf("%s", next_line);
+		   print_next_line = true;
+	    }
+	    else {
+		    printf("End of file ! \n");
+	    }
+
+            input = true;
+	}
+	else {
+		skip = true;
+	}
+
+	if(flag)
+		break;
+
+        if(input) {
+	    while(1) {
+		printf("\n\n\nn/N : next\nq/Q : quit\n\n\n");
+                int ch = getch();
+                if(tolower(ch) == 'n')
+                           break;
+                else if(tolower(ch) == 'q') {
+                           flag = 1;
+                           break;
+                        }
+                else if(!isspace(ch)) {
+                           printf("\n\n\nn/N : next\nq/Q : quit\n\n\n");
+                        }
+	}
+	input = false;
+	}
+    }
+
+    if (!found) {
+        printf("Cannot find pattern\n");
+    }
+
+    fclose(fd);
+
+    return 0;
+   
+}
+
+
